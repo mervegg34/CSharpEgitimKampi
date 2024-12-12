a0c0cd64e961c44340e96813a6ea0c9044bfaeeb
Merve <mervegurler53@gmail.com>
2024-12-12 16:07:33 +0300

    Add files via upload

---
+++ 06_Arrays/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Temel Dizi Örnekleri
+
+            //string[] colors = new string[4];
+            //colors[0] = "Kırmızı";
+            //colors[1] = "Beyaz";
+            //colors[2] = "Sarı";
+            //colors[3] = "Mavi";
+            //Console.WriteLine(colors[2]);
+
+            ///////////////////////////////////
+
+            //string[] cities = new string[5];
+            //cities[0] = "Milano";
+            //cities[1] = "Budapeşte";
+            //cities[2] = "Lyon";
+            //cities[3] = "Kahire";
+            //cities[4] = "Üsküp";
+            //Console.WriteLine(cities[3]);
+
+            ///////////////////////////////////
+
+            //string[] cities = { "Prag", "İstanbul", "Atina", "Bursa", "Roma" };
+            //Console.WriteLine(cities[2]);
+
+            #endregion
+
+            #region Dizideki Tüm Elemanları Listeleme
+
+            //string[] colors = { "Kırmızı", "Sarı", "Turuncu", "Yeşil", "Mavi", "Pembe", "Beyaz", "Siyah" };
+            //for (int i = 0; i < colors.Length; i++)
+            //{
+            //    Console.WriteLine(colors[i]); 
+            //}
+
+            ///////////////////////////////////
+
+            //int [] numbers = { 125, 785, 6849, 235, 41525, 64, 72, 936 };
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 3 == 0)
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+            //}
+
+            ///////////////////////////////////
+
+            //int[] myArray = { 75, 29, 289, 2654, 3654, 45, 96944, 7848956, 25, 1 };
+            //int maxNumber = myArray[0];
+            //for (int i = 0; i < myArray.Length; i++)
+            //{
+            //    if (myArray[i] > maxNumber)
+            //    {
+            //        maxNumber = myArray[i];
+            //    }
+            //} Console.WriteLine(maxNumber);
+
+            ///////////////////////////////////
+
+            //int[] numbers = { 78, 52, 46, 23, 956, 100, 789 };
+            //Array.Sort(numbers);
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            ///////////////////////////////////
+
+            //int[] numbers = { 78, 52, 46, 23, 956, 100, 789 };
+            //Array.Reverse(numbers);
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            #endregion
+
+            #region Dizi Metotlar
+
+            //string[] customers = { "ali", "enes", "merve", "sena", "ayşe" };
+            //int index = Array.IndexOf(customers, "merve");
+            //Console.WriteLine(index);
+
+            ////////////////////////////
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            //Console.WriteLine("En Büyük Sayı: " +numbers.Max()+ "  En Küçük Sayı: " +numbers.Min());
+
+            #endregion
+
+            #region Kullanıcıdan Değer Alma
+
+            //string[] cities = new string[5];
+            //for (int i = 0; i < cities.Length; i++)
+            //{
+            //    Console.Write($"Lütfen {i + 1}. Şehri Giriniz: ");
+            //    cities[i] = Console.ReadLine();
+            //}
+
+            //////////////////////////////////////////
+
+            //int[] numbers = { 10, 20, 30, 50, 60 };
+            //int sum = 0;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    sum += numbers[i];
+            //}
+            //Console.WriteLine(sum);
+
+            //////////////////////////////////////////
+
+            //int[] numbers = { 17, 85, 64, 325, 423, 672, 96 };
+
+            //Console.WriteLine("Çift Sayılar");
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+            //}
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("Tek Sayılar");
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 1)
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+            //}
+
+            #endregion
+
+        }
+    }
+}
---
+++ 08_Methods/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Void Metotlar
+
+            //geriye değer döndürmeyen metotlar
+
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Merve ");
+            //    Console.WriteLine("Ahmet");
+            //    Console.WriteLine("Mehmet");
+            //    Console.WriteLine("Enes");
+            //}
+
+            //CustomerList();
+            //CustomerList();
+            //CustomerList();
+            //CustomerList();
+
+            //void Sum()
+            //{
+            //    int x = 1;
+            //    int y = 2;
+            //    int z = x + y;
+            //    Console.WriteLine(z);
+            //}
+            //Sum();
+
+            #endregion
+
+            #region Geriye Değer Döndürmeyen Parametreli Metotlar
+
+            //void WriteMethod(string customerName)
+            //{
+            //    Console.WriteLine(customerName);
+            //}
+            //WriteMethod("Merve Gürler");
+
+            //void CustomerCard(string name, string surName)
+            //{
+            //    Console.WriteLine("Müşteri: " +name+ " " +surName);
+            //}
+            //CustomerCard("Merve", "Gürler");
+            //CustomerCard("Sena", "Gürler");
+
+            #endregion
+
+            #region Geriye Değer Döndürmeyen Int Parametreli Metotlar
+
+            //void Sum(int number1, int number2, int number3)
+            //{
+            //    int result = number1 + number2 + number3;
+            //    Console.WriteLine(result);
+            //}
+            //Sum(1 ,2, 3);
+
+            #endregion
+
+            #region Geriye Değer Döndüren Metotlar
+
+            //string StudentCard()
+            //{
+            //    string name = "Merve";
+            //    string surname = "Gürler";
+            //    return name + " " + surname;
+            //}
+            //Console.WriteLine(StudentCard());
+
+            #endregion
+
+            #region Geriye Değer Döndüren String Parametreli Metotlar
+
+            //string CountryCard(string countryName, string capital, string flagColor)
+            //{
+            //    string cardInfo = "Ülke: " + countryName + " -Başkent: " + capital + " -Bayrak Rengi: " + flagColor;
+            //    return cardInfo;
+            //}
+            //string x, y, z;
+            //Console.Write("Ülke adını giriniz: ");
+            //x = Console.ReadLine();
+            //Console.Write("Başkenti giriniz: ");
+            //y = Console.ReadLine();
+            //Console.Write("Bayrak rengini giriniz: ");
+            //z = Console.ReadLine();
+
+            //Console.WriteLine(CountryCard(x, y, z));
+
+            #endregion
+
+            #region Geriye Değer Döndüren Int Parametreli Metotlar
+
+            //int Sum(int number1, int number2)
+            //{
+            //    int result = number1 + number2;
+            //    return result;
+            //}
+            //Console.WriteLine(Sum(45, 95));
+            //Console.WriteLine(Sum(12, 56));
+            //Console.WriteLine(Sum(70, 38));
+            //Console.WriteLine(Sum(24, 55));
+
+            #endregion
+
+            #region Örnek Uygulama
+
+            //string ExamResult(string student, int exam1, int exam2, int exam3)
+            //{
+            //    int result = (exam1 + exam2 + exam3) /3;
+            //    if (result >=50)
+            //    {
+            //        return student + " isimli öğrenci sınavı geçti. " + " Ortalama: " + result; 
+            //    }
+            //    else
+            //    {
+            //        return student + " isimli öğrenci sınavı geçemedi. " + " Ortalama: " + result;
+            //    }
+            //}
+            //Console.WriteLine(ExamResult("Merve", 50, 85, 14));
+            //Console.WriteLine(ExamResult("Sena", 20, 73, 100));
+            //Console.WriteLine(ExamResult("Enes", 63, 64, 89));
+
+            #endregion
+
+        }
+    }
+}
